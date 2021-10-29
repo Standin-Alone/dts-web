a0b6e8d73cd6648138fbca3a995eb17cc277b59e
Standin-Alone <johnedcelzenarosa@gmail.com>
2021-10-29 09:46:35 +0800

    push

---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config = array(
+    'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
+    'smtp_host' => 'ssl://smtp.gmail.com', 
+    'smtp_port' => 465,
+    'smtp_user' => 'support.sadd@da.gov.ph',
+    'smtp_pass' => 'SysADDem@1L0721',    
+    'mailtype' => 'html', //plaintext 'text' mails or 'html'    
+    'charset' => 'iso-8859-1',
+    'newline' => "\r\n",
+);
